2babdcb725b028b5844bfe27ed91240a528d4ef1
BRAAMHMDHMDA <baraa.mhmd.hmda.4@gmail.com>
2025-01-04 13:26:22 +0200

    Fix Errors

--- resources/views/front/home/sections/_rooms.blade.php
+++ resources/views/front/home/sections/_rooms.blade.php
@@ -12,7 +12,7 @@
                         <div class="row align-items-center">
                             <div class="col-lg-5 col-md-4 p-0">
                                 <div class="room-card-img">
-                                    <a href="#" >
+                                    <a wire:navigate href="{{ route('room.show', $room->slug) }}" >
                                         <img src="{{ $room->image_url }}" alt="Images">
                                     </a>
                                 </div>
@@ -21,7 +21,7 @@
                             <div class="col-lg-7 col-md-8 p-0">
                                 <div class="room-card-content">
                                     <h3>
-                                        <a href="#">{{ $room->name }}</a>
+                                        <a wire:navigate href="{{ route('room.show', $room->slug) }}" >{{ $room->name }}</a>
                                     </h3>
                                     <span>{{$room->price}}$ / Per Night </span>
                                     <div class="rating">
@@ -44,7 +44,7 @@
                                         <li ><i class='bx bxs-hotel'></i> {{$room->bed_style}}</li>
                                     </ul>
 
-                                    <a href="{{ route('room.show', $room->slug) }}" class="book-more-btn">
+                                    <a wire:navigate href="{{ route('room.show', $room->slug) }}" class="book-more-btn">
                                         Book Now
                                     </a>
                                 </div>
--- resources/views/front/pages/about.blade.php
+++ resources/views/front/pages/about.blade.php
@@ -5,6 +5,49 @@
     <!-- Inner Banner End -->
 
 
+    <!-- Ability Area -->
+    <div class="ability-area  pt-100 pb-70">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="ability-content">
+                        <div class="section-title">
+                            <h2>Our Ability to the Global and International Market</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ante tellus,
+                                sit amet rhoncus massa aliquam sit amet. Cras porttitor mauris quis mattis ornare.
+                                In efficitur at sem quis pretium. Aenean sit amet neque ut dolor lacinia rutrum.
+                                In vulputate pellentesque turpis et porta.
+                            </p>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-6">
+                                <div class="ability-counter">
+                                    <h3 class="text-color">14K</h3>
+                                    <p>5 Star Retting Reviews</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-sm-6">
+                                <div class="ability-counter">
+                                    <h3 class="text-color">225K</h3>
+                                    <p>Items Served Breakfast</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="ability-img-2">
+                        <img src="{{ asset('front_assets/img/ability-img2.jpg') }}" alt="Images">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Ability Area  End -->
+
     <!-- Choose Area -->
     <div class="choose-area section-bg pb-70 pt-100">
         <div class="container">
@@ -49,51 +92,8 @@
     </div>
     <!-- Choose Area End -->
 
-    <!-- Ability Area -->
-    <div class="ability-area pt-100 pb-70">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <div class="ability-content">
-                        <div class="section-title">
-                            <h2>Our Ability to the Global and International Market</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt ante tellus,
-                                sit amet rhoncus massa aliquam sit amet. Cras porttitor mauris quis mattis ornare.
-                                In efficitur at sem quis pretium. Aenean sit amet neque ut dolor lacinia rutrum.
-                                In vulputate pellentesque turpis et porta.
-                            </p>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-sm-6">
-                                <div class="ability-counter">
-                                    <h3 class="text-color">14K</h3>
-                                    <p>5 Star Retting Reviews</p>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6 col-sm-6">
-                                <div class="ability-counter">
-                                    <h3 class="text-color">225K</h3>
-                                    <p>Items Served Breakfast</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="ability-img-2">
-                        <img src="{{ asset('front_assets/img/ability-img2.jpg') }}" alt="Images">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Ability Area  End -->
-
     <!-- Specialty Area Two -->
-    <div class="specialty-area section-bg pt-100 pb-70">
+    <div class="specialty-area pt-100 pb-70">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6">
